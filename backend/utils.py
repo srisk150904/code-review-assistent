8cb9142dbc26207bd22d7a49fe469d78a769f39c
SRIRAM 22BCE3761 <128880172+srisk150904@users.noreply.github.com>
2025-10-17 12:03:43 +0530

    Delete backend directory

--- backend/utils.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Dict
-from fastapi import UploadFile
-
-async def read_files(files: List[UploadFile]) -> List[Dict[str, str]]:
-    out = []
-    for f in files:
-        content = (await f.read()).decode("utf-8", errors="replace")
-        out.append({"filename": f.filename, "content": content})
-    return out
